ad61b5dde2781422cc6d160908125ceaff3e3ca1
nnecec <nnecec@outlook.com>
2018-04-10 00:47:30 +0800

    update

--- interview/Function/deepCopy/deepCopy.js
+++ interview/Function/deepCopy/deepCopy.js
@@ -19,21 +19,20 @@ const deepCopy = (source, target) => {
 	}
 	// 遍历需要拷贝的源对象
 	for (key in source) {
-		// 如果对应key的值是数组或对象
+		// 如果对应key的值是数组或对象 则进入深拷贝的循环
 		if (isObject(source[key] && isArray(source[key]))) {
 
-			// 初始化 target[key]
+			// 初始化 target[key] 为对应的 数组 或 对象
 			if (!isArray(target[key]) && isArray(source[key])) target[key] = []
 			if (!isObject(target[key]) && isObject(source[key])) target[key] = {}
 
-			// 递归
+			// 递归 将 source[key] 的值往 target[key]上 deepCopy
 			deepCopy(source[key], target[key])
 		} else if (source[key]) { //如果不是引用类型 且有值
 			target[key] = source[key]
 		}
 	}
 	return target
-
 }
 
 module.exports = deepCopy
--- interview/Function/deepCopy/deepCopy.test.js
+++ interview/Function/deepCopy/deepCopy.test.js
@@ -2,18 +2,18 @@ const test = require('ava')
 const deepCopy = require('./deepCopy')
 
 test('deepCopy array', t => {
-	let initialValue = ['abc', 123, { a: 'a', b: 'b' }, true, [1, 23, 4]]
+	let initialValue = ['abc', 123, { a: 'a', b: 'b', c: { c1: 1 } }, true, [1, 23, 4]]
 	const copyValue = deepCopy(initialValue)
 	t.deepEqual(initialValue, copyValue)
 	initialValue = [3, 2, 1]
-	t.deepEqual(copyValue, ['abc', 123, { a: 'a', b: 'b' }, true, [1, 23, 4]])
+	t.deepEqual(copyValue, ['abc', 123, { a: 'a', b: 'b', c: { c1: 1 } }, true, [1, 23, 4]])
 })
 
 test('deepCopy object', t => {
 	let initialValue = {
 		a: 'abc',
 		b: 123,
-		c: { a: 'a', b: 'b' },
+		c: { a: 'a', b: 'b', c: { c1: 1 } },
 		d: [1, 23, 4]
 	}
 	const copyValue = deepCopy(initialValue)
@@ -22,7 +22,7 @@ test('deepCopy object', t => {
 	t.deepEqual(copyValue, {
 		a: 'abc',
 		b: 123,
-		c: { a: 'a', b: 'b' },
+		c: { a: 'a', b: 'b', c: { c1: 1 } },
 		d: [1, 23, 4]
 	})
 })
