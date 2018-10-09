f62d687f523af791fc949d53198684145f9b9690
nnecec <nnecec@outlook.com>
2018-10-09 19:06:33 +0800

    add promise test

--- JavaScript/Promise/promise/utils.js
+++ JavaScript/Promise/promise/utils.js
@@ -2,6 +2,11 @@ function isFunction(x) {
   return typeof x === 'function'
 }
 
+function isArray(x) {
+  return Object.prototype.toString.call(x) === '[object Array]'
+}
+
 module.exports = {
-  isFunction: isFunction
+  isFunction: isFunction,
+  isArray: isArray,
 }
