0c52adfb52922f33641340347bab8e665a89e539
nnecec <nnecec@outlook.com>
2018-10-09 16:52:44 +0800

    update

--- JavaScript/Promise/promise/internal.js
+++ JavaScript/Promise/promise/internal.js
@@ -1,12 +1,16 @@
 var immediate = require('immediate')
-var utils = require('../utils')
-var asap = require('./asap')
+var utils = require('./utils')
 
 
 var PENDING = 0
-var FULFILLED = 1,
+var FULFILLED = 1
 var REJECTED = 2
 var handlers = {}
+if (!process.browser) {
+  // in which we actually take advantage of JS scoping
+  var UNHANDLED = ['UNHANDLED'];
+}
+
 
 /**
  * 添加到 parent 队列中，并绑定 resolve reject 方法
@@ -44,12 +48,12 @@ function unwrap(promise, func, value) {
   immediate(function () {
     var returnValue
     try {
-      returnValue = func(value)
+      returnValue = func(value) // 对当前 then 中的值调用 resolver 方法获得 returnValue
     } catch (e) {
       return handlers.reject(promise, e)
     }
 
-    // 如果返回值 与 promise 一样，则说明 promise 返回了一个 promise
+    // 如果返回值 与 promise 一样，则说明 promise 返回了自身
     if (returnValue === promise) {
       handlers.reject(promise, new TypeError('Cannot resolve promise with itself'))
     } else {
@@ -59,26 +63,31 @@ function unwrap(promise, func, value) {
 }
 
 handlers.resolve = function (self, value) {
-  var result = tryCatch(getThen, value);
+  // TODO: 为什么 tryCatch getThen
+  var result = tryCatch(getThen, value)
 
-  // 如果
+  // 如果有 error，则进入 reject
   if (result.status === 'error') {
     return handlers.reject(self, result.value);
   }
+  // 如果没有 error
   var thenable = result.value;
 
   if (thenable) {
     safelyResolveThenable(self, thenable);
   } else {
+    // promise 的状态修改为 Fulfilled 当前值修改为 value
     self._state = FULFILLED;
-    self._outcome = value;
-    var i = -1;
-    var len = self.queue.length;
+    self._outcome = value
+
+    // 
+    var i = -1
+    var len = self._subscribers.length
     while (++i < len) {
-      self.queue[i].callFulfilled(value);
+      self._subscribers[i].callFulfilled(value)
     }
   }
-  return self;
+  return self
 }
 
 handlers.reject = function (self, error) {
@@ -89,39 +98,46 @@ handlers.reject = function (self, error) {
     if (self.handled === UNHANDLED) {
       immediate(function () {
         if (self.handled === UNHANDLED) {
-          process.emit('unhandledRejection', error, self);
+          process.emit('unhandledRejection', error, self)
         }
-      });
+      })
     }
   }
 
-  var i = -1;
-  var len = self._subscribers.length;
+  var i = -1
+  var len = self._subscribers.length
   while (++i < len) {
-    self._subscribers[i].callRejected(error);
+    self._subscribers[i].callRejected(error)
   }
-  return self;
+  return self
 }
 
+/**
+ * try...catch 调用 func 及其参数 value
+ *
+ * @param {Function} func
+ * @param {any} value
+ * @returns
+ */
 function tryCatch(func, value) {
-  var out = {};
+  var out = {}
   try {
-    out.value = func(value);
-    out.status = 'success';
+    out.value = func(value)
+    out.status = 'success'
   } catch (e) {
-    out.status = 'error';
-    out.value = e;
+    out.status = 'error'
+    out.value = e
   }
-  return out;
+  return out
 }
 
 function getThen(obj) {
   // Make sure we only access the accessor once as required by the spec
-  var then = obj && obj.then;
+  var then = obj && obj.then
   if (obj && (typeof obj === 'object' || typeof obj === 'function') && typeof then === 'function') {
     return function appyThen() {
-      then.apply(obj, arguments);
-    };
+      then.apply(obj, arguments)
+    }
   }
 }
 
